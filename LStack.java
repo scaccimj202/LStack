08e51334e216a1843ef48960696eaaced3226686
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-29 13:19:42 -0400

    Initial commit

---
+++ LStack.java
@@ -0,0 +1,7 @@
+/**
+ *@author Matthew Scaccia
+ *course CIS303
+ *@since 3/29/2020
+ *@assignment 4b
+ *description: LStack data structure adapted from Clifford Shaffers Data Structures and Algorithim Analysis, Chapter 4.2.2
+ */

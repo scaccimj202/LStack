6d49ab566414fb14ea1369951734564d156e2e74
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-29 13:24:44 -0400

    Added constructors to LStack

--- LStack.java
+++ LStack.java
@@ -5,3 +5,22 @@
  *@assignment 4b
  *description: LStack data structure adapted from Clifford Shaffers Data Structures and Algorithim Analysis, Chapter 4.2.2
  */
+
+ public class LStack<E> implements StackADT<E>{
+     private Node<E> top;
+     private int size;
+
+     /**
+      * Constructors, for the LStack both ignore the size input and 
+      *default to zero
+      */
+     public LStack(){
+         top = null;
+         size = 0;
+     }
+
+     public LStack(int size_){
+         top = null;
+         size = 0;
+     }
+ }
